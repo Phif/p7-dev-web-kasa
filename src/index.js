eee704b91ee0e35fab966dd67aa0c98104a34870
Phif <71464584+Phif@users.noreply.github.com>
2023-08-21 15:53:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Router from './Router';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Router />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
